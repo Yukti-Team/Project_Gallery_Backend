e0a17cad1912ee94765c5a82cd6492c5d3d467ab
rutuja369 <rrkhilare02@gmail.com>
2023-04-17 00:38:29 +0530

    .

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require("cors");
 const connectDatabase = require("./database/connection");
-
-
 const app = express();
 
 app.use(express.json());
