dacdedf2f9944aed323d09a5abfa4f4ea3d1bf94
SUYOG PATIL <suyog732002@gmail.com>
2023-04-26 00:12:08 +0530

    User backend change

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const {login, signUp, getUser, updateUser} = require("../controller/userController");
+const { login, signUp, getUser, updateUser, checkUserName, deleteUser } = require("../controller/userController");
 const Router = express.Router();
 
 Router.route("/signup").post(signUp);
 Router.route("/login").post(login);
 Router.route("/:id").get(getUser);
 Router.route("/update/:id").patch(updateUser);
-
+Router.route('/delete/:id').delete(deleteUser);
+Router.route('/check-username').post(checkUserName);
 
 //changepwd, updateprofile, getuserdata 
 
--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,20 @@
 const userSc = require("../schema/user_schema");
 const bcrypt = require('bcrypt');
 
+// Route for checking if username is unique
+exports.checkUserName = async (req, res) => {
+  try {
+    const user = await userSc.findOne({ username: req.body.username });
+    if (user) {
+      res.json({ success: false, message: 'Username is already taken' });
+    } else {
+      res.json({ success: true, message: 'Username is available' });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+}
+
 exports.signUp = async (req, res) => {
   try {
     const salt = await bcrypt.genSalt();
@@ -14,64 +28,75 @@ exports.signUp = async (req, res) => {
 
     try {
       let dataToStore = await userSc.create(user);
-      //deleting password from data for security
       dataToStore = dataToStore.toObject();
       delete dataToStore.password;
       console.log(dataToStore);
-      res.status(200).json(dataToStore);
+      res.status(200).json({
+        statusCode: 200,
+        user: dataToStore
+      });
 
-      
+
     } catch (error) {
       if (error.code === 11000) {
-        res.status(400).json({
-          status: "error",
-          message: "Email or username already exists",
+        res.status(401).json({
+          statusCode: 401,
+          message: "userSc with this email already exists please try to login",
         });
       } else {
         res.status(400).json({
-          status: "error",
+          statusCode: 400,
           message: error.message,
         });
       }
     }
   } catch (error) {
     res.status(500).send({
-      message: "Something went wrong...",
+      statusCode: 500,
+      message: "Internal server error",
     });
   }
 }
 
-
-
 exports.login = async (req, res) => {
+  const searchQuery = req.body.email; // It can be email/username
+  const password = req.body.password;
 
-  const { email, password } = req.body;
+  const user1 = await userSc.findOne({ email: searchQuery });
+  const user2 = await userSc.findOne({ username: searchQuery });
 
+  if (user1 === null && user2 === null) {
+    return res.status(400).send({
+      statusCode: 400,
+      message: "Invalid email or password",
+    });
+  }
+  try {
+    const user = (user1 === null) ? user2 : user1; 
 
-    const user = await userSc.findOne({ email });
-    if (user == null) {
-      res.status(400).send({
-        "message": "Invalid email or password"
-      })
+    console.log(user);
+
+    if (await bcrypt.compare(password, user.password)) {
+      const userObject = user.toObject();
+      delete userObject.password;
+      console.log(userObject);
+      return res.status(200).send({
+        user: userObject,
+        statusCode: 200,
+      });
+    } else {
+      return res.status(401).send({
+        statusCode: 401,
+        message: "Username or Password is incorrect",
+      });
     }
-    try {
-      if (await bcrypt.compare(password, user.password)) {
-        //i want to remove this passwrod field from user object before sending it to frontend
-        const userObject = user.toObject();
-        delete userObject.password;
-        console.log(userObject);
-        res.status(200).send(userObject);
-      } else {
-        res.send("Not allowed");
-      }
-  
-    } catch (error) {
-      res.status(500).send({
-        "message": "Something went wrong..."
-      })
-    }
-  
-}
+  } catch (error) {
+    return res.status(500).send({
+      statusCode: 500,
+      message: "Internal server error",
+    });
+  }
+};
 
 exports.getUser = async (req, res) => {
 
@@ -109,10 +134,36 @@ exports.updateUser = async (req, res) => {
   let options = { new: true };
 
   try {
-      const data = await userSc.findByIdAndUpdate(id, updatedData, options);
+    const data = await userSc.findByIdAndUpdate(id, updatedData, options);
 
-      res.status(201).send(data);
+    res.status(201).send(data);
   } catch (error) {
-      res.status().send(error.message);
+    res.status().send(error.message);
+  }
+}
+
+exports.deleteUser = async (req, res) => {
+  let id = req.params.id;
+
+  if (id === "all") {
+    const data = await userSc.deleteMany({});
+    res.status(200).send({
+      data
+    });
+  }
+  else {
+    try {
+      const data = await userSc.findByIdAndDelete(id);
+      res.json({
+        "status_code": 204,
+        "status": `Deleted the user ${data.username} with id ${data.id} from db`,
+      });
+    }
+    catch (error) {
+      console.log(error);
+      res.status(404).json({
+        message: "Not found "
+      })
+    }
   }
 }
